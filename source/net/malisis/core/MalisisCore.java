19d3c2e5e584a85bf5881a8e1de87bfaf12a8dc7
Ordinastie <ordinastie@hotmail.com>
2014-05-17 17:55:22 +0200

    Version 1.7.2-0.7.1

--- source/net/malisis/core/MalisisCore.java
+++ source/net/malisis/core/MalisisCore.java
@@ -37,7 +37,7 @@ public class MalisisCore extends DummyModContainer
 {
 	public static final String modid = "malisiscore";
 	public static final String modname = "Malisis Core";
-	public static final String version = "1.7.2-0.7";
+	public static final String version = "1.7.2-0.7.1";
 	public static final String url = "";
 	public static File coremodLocation;
 
