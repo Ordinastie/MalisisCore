9081b437e0874b92c0bd5de35359daec203a7732
Ordinastie <ordinastie@hotmail.com>
2014-07-13 14:59:51 +0200

    Removed casting to EntityClientMP for UserAttackEvent

--- source/net/malisis/core/event/user/UserAttackEvent.java
+++ source/net/malisis/core/event/user/UserAttackEvent.java
@@ -1,17 +1,16 @@
 package net.malisis.core.event.user;
 
-import net.minecraft.client.entity.EntityClientPlayerMP;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 
 public class UserAttackEvent extends UserEvent
 {
-	public EntityClientPlayerMP player;
+	public EntityPlayer player;
 	public Entity target;
-	
+
 	public UserAttackEvent(EntityPlayer player, Entity target)
 	{
-		this.player = (EntityClientPlayerMP) player;
+		this.player = player;
 		this.target = target;
 	}
 }
