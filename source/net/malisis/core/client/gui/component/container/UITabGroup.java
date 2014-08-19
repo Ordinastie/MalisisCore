6f47e6c5ca186539c52bd6f99c157c961a742ca0
Ordinastie <ordinastie@hotmail.com>
2014-08-19 08:37:10 +0200

    Added GUI tabs

---
+++ source/net/malisis/core/client/gui/component/container/UITabGroup.java
@@ -0,0 +1,94 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Ordinastie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package net.malisis.core.client.gui.component.container;
+
+import java.util.HashMap;
+
+import net.malisis.core.client.gui.component.interaction.UITab;
+
+/**
+ * @author Ordinastie
+ * 
+ */
+public class UITabGroup extends UIContainer<UITabGroup>
+{
+	private HashMap<UITab, UIContainer> listTabs = new HashMap<>();
+	private UITab activeTab;
+
+	public UITabGroup()
+	{
+		this.height = 19;
+		this.width = 0;
+		this.x = 3;
+	}
+
+	@Override
+	public UITabGroup setPosition(int x, int y, int anchor)
+	{
+		return super.setPosition(x + 3, y, anchor);
+	}
+
+	/**
+	 * Adds tab and its corresponding container to this <code>UITabGroup</code>.<br />
+	 * Also sets the width of this <code>UITabGroup</code>.
+	 * 
+	 * @param tab
+	 * @param container
+	 */
+	public void addTab(UITab tab, UIContainer container)
+	{
+		add(tab);
+		tab.setPosition(getWidth(), 0);
+		tab.setContainer(container);
+		tab.setActive(false);
+		listTabs.put(tab, container);
+		width += tab.getWidth();
+
+		if (activeTab == null)
+			setActiveTab(tab);
+	}
+
+	public void addTab(String tabName, UIContainer container)
+	{
+		addTab(new UITab(tabName), container);
+	}
+
+	/**
+	 * Activates the tab and deactivates currently active tab.
+	 * 
+	 * @param tab
+	 */
+	public void setActiveTab(UITab tab)
+	{
+		if (activeTab == tab)
+			return;
+
+		if (activeTab != null)
+			activeTab.setActive(false);
+
+		activeTab = tab;
+		tab.setActive(true);
+	}
+}
---
+++ source/net/malisis/core/client/gui/component/interaction/UITab.java
@@ -0,0 +1,145 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Ordinastie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package net.malisis.core.client.gui.component.interaction;
+
+import java.util.Arrays;
+
+import net.malisis.core.client.gui.GuiIcon;
+import net.malisis.core.client.gui.GuiRenderer;
+import net.malisis.core.client.gui.component.UIComponent;
+import net.malisis.core.client.gui.component.container.UIContainer;
+import net.malisis.core.client.gui.component.container.UITabGroup;
+import net.malisis.core.client.gui.event.MouseEvent;
+import net.malisis.core.renderer.RenderParameters;
+import net.malisis.core.renderer.element.Shape;
+import net.malisis.core.renderer.preset.ShapePreset;
+import net.malisis.core.util.MouseButton;
+
+import com.google.common.eventbus.Subscribe;
+
+/**
+ * @author Ordinastie
+ * 
+ */
+public class UITab extends UIComponent<UITab>
+{
+	//@formatter:off
+	public static GuiIcon[] icons = new GuiIcon[] { new GuiIcon(200, 	15, 	5, 	5),
+													new GuiIcon(205, 	15, 	5, 	5),
+													new GuiIcon(210, 	15, 	5, 	5),
+													new GuiIcon(200, 	20, 	5, 	5),
+													new GuiIcon(205, 	20, 	5, 	5),
+													new GuiIcon(210, 	20, 	5, 	5)};
+	//@formatter:on
+
+	protected String label;
+	protected boolean autoWidth = false;
+	protected UIContainer container;
+	protected boolean active = false;
+
+	public UITab(int width, String label)
+	{
+		setSize(width, 0);
+		setLabel(label);
+	}
+
+	public UITab(String label)
+	{
+		this(0, label);
+	}
+
+	@Override
+	public UITab setSize(int width, int height)
+	{
+		this.height = 17;
+		this.width = width;
+		if (width == 0)
+			autoWidth = true;
+		return this;
+	}
+
+	@Override
+	public UITab setPosition(int x, int y, int anchor)
+	{
+		return super.setPosition(x, y + 1, anchor);
+	}
+
+	public UITab setLabel(String label)
+	{
+		this.label = label;
+		if (autoWidth)
+			width = GuiRenderer.getStringWidth(label) + 8;
+		return this;
+	}
+
+	public UITab setContainer(UIContainer container)
+	{
+		this.container = container;
+		return this;
+	}
+
+	public void setActive(boolean active)
+	{
+		if (this.active != active)
+		{
+			this.y += active ? -1 : 1;
+			this.height += active ? 1 : -1;
+		}
+
+		this.active = active;
+		this.container.setVisible(active);
+		this.container.setDisabled(!active);
+		this.zIndex = active ? container.getZIndex() + 1 : 0;
+	}
+
+	@Subscribe
+	public void onClick(MouseEvent.Release event)
+	{
+		if (event.getButton() != MouseButton.LEFT)
+			return;
+
+		if (!(parent instanceof UITabGroup))
+			return;
+
+		((UITabGroup) parent).setActiveTab(this);
+	}
+
+	@Override
+	public void drawBackground(GuiRenderer renderer, int mouseX, int mouseY, float partialTick)
+	{
+		Shape shape = new Shape(Arrays.copyOf(ShapePreset.GuiXYResizable(width, height).getFaces(), 6));
+		RenderParameters rp = new RenderParameters();
+		renderer.drawShape(shape, rp, icons);
+	}
+
+	@Override
+	public void drawForeground(GuiRenderer renderer, int mouseX, int mouseY, float partialTick)
+	{
+		int x = (width - GuiRenderer.getStringWidth(label)) / 2;
+		int y = 3;
+		int color = isHovered() ? 0xFFFFA0 : (active ? 0xFFFFFF : 0x404040);
+		renderer.drawText(label, x, y, zIndex, color, active);
+	}
+}
