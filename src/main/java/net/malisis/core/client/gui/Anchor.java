365a8f5ba1a4767b46833b456910e2ff846de66f
Ordinastie <ordinastie@hotmail.com>
2015-02-24 15:10:03 +0100

    Changed Anchor.NONE to TOP | LEFT

--- src/main/java/net/malisis/core/client/gui/Anchor.java
+++ src/main/java/net/malisis/core/client/gui/Anchor.java
@@ -26,13 +26,14 @@ package net.malisis.core.client.gui;
 
 public class Anchor
 {
-	public static int NONE = 0;
 	public static int TOP = 1;
 	public static int BOTTOM = 2;
-	public static int MIDDLE = 3;
+	public static int MIDDLE = TOP | BOTTOM;
 	public static int LEFT = 4;
 	public static int RIGHT = 8;
-	public static int CENTER = 12;
+	public static int CENTER = LEFT | RIGHT;
+
+	public static int NONE = TOP | LEFT;
 
 	public static int horizontal(int anchor)
 	{
