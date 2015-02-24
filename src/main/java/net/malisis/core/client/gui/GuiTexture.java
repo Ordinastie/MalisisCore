4b5981dd292c1cd1c9b614b0763244c711f04b3f
Ordinastie <ordinastie@hotmail.com>
2015-02-24 11:51:46 +0100

    Added possiblity to make GuiTextures from File or BufferedImage

--- src/main/java/net/malisis/core/client/gui/GuiTexture.java
+++ src/main/java/net/malisis/core/client/gui/GuiTexture.java
@@ -24,13 +24,26 @@
 
 package net.malisis.core.client.gui;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import net.malisis.core.client.gui.icon.GuiIcon;
 import net.malisis.core.renderer.icon.MalisisIcon;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.DynamicTexture;
 import net.minecraft.util.ResourceLocation;
 
 /**
- * @author Ordinastie
+ * The Class GuiTexture represents the textures loaded and to be drawn inside a {@link MalisisGui}.<br>
+ * The textures can used from a {@link ResourceLocation} if the resource is inside the project, from a {@link File} or directly from a
+ * {@link BufferedImage}.<br>
+ * In case of {@code ResourceLocation}, the original dimension should be specified if parts of the texture is to be retrieved as
+ * {@link MalisisIcon}.
  *
+ * @author Ordinastie
  */
 public class GuiTexture
 {
@@ -38,6 +51,13 @@ public class GuiTexture
 	protected int width;
 	protected int height;
 
+	/**
+	 * Instantiates a new {@link GuiTexture}.
+	 *
+	 * @param rl the rl
+	 * @param width the width
+	 * @param height the height
+	 */
 	public GuiTexture(ResourceLocation rl, int width, int height)
 	{
 		this.resourceLocation = rl;
@@ -45,16 +65,82 @@ public class GuiTexture
 		this.height = height;
 	}
 
+	/**
+	 * Instantiates a new {@link GuiTexture}. <br>
+	 * Automatically determines the width and height for the {@link File}.
+	 *
+	 * @param file the file
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	public GuiTexture(File file) throws IOException
+	{
+		this(ImageIO.read(file), file.getName());
+	}
+
+	/**
+	 * Instantiates a new {@link GuiTexture}.<br>
+	 * Automatically determines the width and height for the {@link BufferedImage}.
+	 *
+	 * @param image the image
+	 * @param name the name
+	 */
+	public GuiTexture(BufferedImage image, String name)
+	{
+		DynamicTexture dynTex = new DynamicTexture(image);
+		width = image.getWidth();
+		height = image.getHeight();
+		resourceLocation = Minecraft.getMinecraft().getTextureManager().getDynamicTextureLocation(name, dynTex);
+	}
+
+	/**
+	 * Instantiates a new {@link GuiTexture}.
+	 *
+	 * @param rl the rl
+	 */
 	public GuiTexture(ResourceLocation rl)
 	{
 		this(rl, 1, 1);
 	}
 
+	/**
+	 * Gets the width of this {@link GuiTexture}.
+	 *
+	 * @return the width
+	 */
+	public int getWidth()
+	{
+		return width;
+	}
+
+	/**
+	 * Gets the height of this {@link GuiTexture}.
+	 *
+	 * @return the height
+	 */
+	public int getHeight()
+	{
+		return height;
+	}
+
+	/**
+	 * Gets the {@link ResourceLocation} of this {@link GuiTexture}.
+	 *
+	 * @return the resource location
+	 */
 	public ResourceLocation getResourceLocation()
 	{
 		return this.resourceLocation;
 	}
 
+	/**
+	 * Creates the {@link MalisisIcon} and initializes it.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @param width the width
+	 * @param height the height
+	 * @return the {@link MalisisIcon}
+	 */
 	private MalisisIcon createIcon(int x, int y, int width, int height)
 	{
 		MalisisIcon icon = new MalisisIcon();
@@ -64,11 +150,33 @@ public class GuiTexture
 		return icon;
 	}
 
+	/**
+	 * Gets a {@link GuiIcon} from a single icon (used for single face shapes).
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @param width the width
+	 * @param height the height
+	 * @return the icon
+	 */
 	public GuiIcon getIcon(int x, int y, int width, int height)
 	{
 		return new GuiIcon(createIcon(x, y, width, height));
 	}
 
+	/**
+	 * Gets a {@link GuiIcon} that is resizable from both X and Y axis.<br>
+	 * The {@code GuiIcon} will hold 9 icons that will behave when resized :<br>
+	 * - the top and bottom row will not change in height when resized<br>
+	 * - left left and right row will not change in width when resized<br>
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @param width the width
+	 * @param height the height
+	 * @param corner the corner
+	 * @return the XY resizable icon
+	 */
 	public GuiIcon getXYResizableIcon(int x, int y, int width, int height, int corner)
 	{
 		int w = width - corner * 2;
@@ -93,6 +201,18 @@ public class GuiTexture
 		return new GuiIcon(icons);
 	}
 
+	/**
+	 * Gets a {@link GuiIcon} that is resizable only on the X axis.<br>
+	 * The {@code GuiIcon} will hold 3 icons that will behave when resized :<br>
+	 * - left left and right icon will not change in width when resized<br>
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @param width the width
+	 * @param height the height
+	 * @param side the side
+	 * @return the x resizable icon
+	 */
 	public GuiIcon getXResizableIcon(int x, int y, int width, int height, int side)
 	{
 		int w = width - side * 2;
