716937a2821cb2fc0e0a5f4a62a11516a1aea7fc
Ordinastie <ordinastie@hotmail.com>
2015-03-12 00:53:34 +0100

    Changed static call to clipString

--- src/main/java/net/malisis/core/client/gui/component/interaction/UISelect.java
+++ src/main/java/net/malisis/core/client/gui/component/interaction/UISelect.java
@@ -472,7 +472,7 @@ public class UISelect extends UIComponent<UISelect> implements Iterable<Option>,
 		{
 			String text = getOption(selectedOption).getLabel(labelPattern);
 			if (!StringUtils.isEmpty(text))
-				renderer.drawText(renderer.clipString(text, width - 15), 2, 2, 1, textColor, textShadow, true);
+				renderer.drawText(GuiRenderer.clipString(text, width - 15), 2, 2, 1, textColor, textShadow, true);
 		}
 
 		if (!expanded)
