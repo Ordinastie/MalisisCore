5cbc2752ec356b0df25ce06af00a59d21077155e
Ordinastie <ordinastie@hotmail.com>
2015-07-30 18:16:44 +0200

    Added INVOKEINTERFACE handling

--- src/main/java/net/malisis/core/asm/mappings/McpMethodMapping.java
+++ src/main/java/net/malisis/core/asm/mappings/McpMethodMapping.java
@@ -24,21 +24,20 @@
 
 package net.malisis.core.asm.mappings;
 
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.MethodInsnNode;
 
 public class McpMethodMapping extends McpMapping<MethodInsnNode>
 {
-
 	public McpMethodMapping(String mcp, String srg, String owner, String descriptor)
 	{
 		super(mcp, srg, owner, descriptor);
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public MethodInsnNode getInsnNode(int opcode)
 	{
-		return new MethodInsnNode(opcode, getOwner(), getName(), getDescriptor());
+		return new MethodInsnNode(opcode, getOwner(), getName(), getDescriptor(), opcode == Opcodes.INVOKEINTERFACE);
 	}
 
 }
