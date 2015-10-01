89d0ff7035fbffa131d87d6ce16c62a85b173abb
Ordinastie <ordinastie@hotmail.com>
2015-10-01 23:51:03 +0200

    Added comments

--- src/main/java/net/malisis/core/block/IBlockDirectional.java
+++ src/main/java/net/malisis/core/block/IBlockDirectional.java
@@ -35,41 +35,93 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 
 /**
- * @author Ordinastie
+ * The Interface IBlockDirectional gives default implementations for blocks that have a facing.<br>
+ * If the block extends {@link MalisisBlock}, relevant methods automatically redirect the implement to this interface.
  *
+ * @author Ordinastie
  */
 public interface IBlockDirectional
 {
+	/** The {@link PropertyDirection} direction that is used by default. */
 	public static final PropertyDirection DIRECTION = PropertyDirection.create("direction", EnumFacing.Plane.HORIZONTAL);
 
+	/**
+	 * Creates the {@link BlockState} with the {@link IBlockDirectional#DIRECTION}.
+	 *
+	 * @param block the block
+	 * @return the block state
+	 */
 	public default BlockState createBlockState(Block block)
 	{
 		return new BlockState(block, DIRECTION);
 	}
 
+	/**
+	 * Automatically gets the right {@link IBlockState} based on the <code>placer</code> facing.
+	 *
+	 * @param block the block
+	 * @param world the world
+	 * @param pos the pos
+	 * @param facing the facing
+	 * @param hitX the hit x
+	 * @param hitY the hit y
+	 * @param hitZ the hit z
+	 * @param meta the meta
+	 * @param placer the placer
+	 * @return the i block state
+	 */
 	public default IBlockState onBlockPlaced(Block block, World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
 	{
 		EnumFacing dir = EntityUtils.getEntityFacing(placer).getOpposite();
 		return block.getDefaultState().withProperty(DIRECTION, dir);
 	}
 
+	/**
+	 * Gets the {@link IBlockState} from <code>meta</code>.
+	 *
+	 * @param block the block
+	 * @param meta the meta
+	 * @return the state from meta
+	 */
 	public default IBlockState getStateFromMeta(Block block, int meta)
 	{
 		return block.getDefaultState().withProperty(DIRECTION, EnumFacing.getHorizontal(meta));
 	}
 
+	/**
+	 * Gets the metadata from the {@link IBlockState}.
+	 *
+	 * @param block the block
+	 * @param state the state
+	 * @return the meta from state
+	 */
 	public default int getMetaFromState(Block block, IBlockState state)
 	{
 		return getDirection(state).getHorizontalIndex();
 	}
 
+	/**
+	 * Gets the {@link EnumFacing direction} for the {@link Block} at world coords.
+	 *
+	 * @param world the world
+	 * @param pos the pos
+	 * @return the EnumFacing, null if the block is not {@link IBlockDirectional}
+	 */
 	public default EnumFacing getDirection(World world, BlockPos pos)
 	{
 		return getDirection(world.getBlockState(pos));
 	}
 
+	/**
+	 * Gets the {@link EnumFacing direction} for the {@link IBlockState}
+	 *
+	 * @param state the state
+	 * @return the EnumFacing, null if the block is not {@link IBlockDirectional}
+	 */
 	public default EnumFacing getDirection(IBlockState state)
 	{
+		if (!(state.getBlock() instanceof IBlockDirectional))
+			return null;
 		return (EnumFacing) state.getValue(DIRECTION);
 	}
 }
