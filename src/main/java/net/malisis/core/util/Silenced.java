ac06e8d8a4a977d4c7ce5d52a1b98ce36b26f908
Ordinastie <ordinastie@hotmail.com>
2016-07-21 16:38:42 +0200

    Added additional elements to Silenced

--- src/main/java/net/malisis/core/util/Silenced.java
+++ src/main/java/net/malisis/core/util/Silenced.java
@@ -56,6 +56,44 @@ public class Silenced
 		}
 	}
 
+	public static <T> boolean accept(Consumer<T> consumer, T t)
+	{
+		try
+		{
+			consumer.accept(t);
+			return true;
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+	}
+
+	public static <T, U> boolean accept(BiConsumer<T, U> biconsumer, T t, U u)
+	{
+		try
+		{
+			biconsumer.accept(t, u);
+			return true;
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+	}
+
+	public static <T, U> T apply(Function<T, U> function, U u)
+	{
+		try
+		{
+			return function.apply(u);
+		}
+		catch (Exception e)
+		{
+			return null;
+		}
+	}
+
 	public static interface Supplier<T>
 	{
 		public T get() throws Exception;
@@ -64,5 +102,20 @@ public class Silenced
 	public static interface Exec
 	{
 		public void exec() throws Exception;
+	}
+
+	public static interface Consumer<T>
+	{
+		public void accept(T t) throws Exception;
+	}
+
+	public static interface BiConsumer<T, U>
+	{
+		public void accept(T t, U u) throws Exception;
+	}
+
+	public static interface Function<T, U>
+	{
+		public T apply(U u) throws Exception;
 	}
 }
