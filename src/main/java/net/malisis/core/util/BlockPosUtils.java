3f029b0214305d8c675b01523ac49e93b1f7f58d
Ordinastie <ordinastie@hotmail.com>
2016-09-29 19:31:17 +0200

    Added compare methods

--- src/main/java/net/malisis/core/util/BlockPosUtils.java
+++ src/main/java/net/malisis/core/util/BlockPosUtils.java
@@ -30,11 +30,20 @@ import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 
 /**
+ * Utility class to handle different interactions with {@link BlockPos}.
+ *
  * @author Ordinastie
  *
  */
 public class BlockPosUtils
 {
+	/**
+	 * Rotates the {@link BlockPos} around the Y axis around the origin (0,0,0).
+	 *
+	 * @param pos the pos
+	 * @param rotation the rotation
+	 * @return the block pos
+	 */
 	public static BlockPos rotate(BlockPos pos, int rotation)
 	{
 		int[] cos = { 1, 0, -1, 0 };
@@ -48,11 +57,23 @@ public class BlockPosUtils
 		return new BlockPos(newX, pos.getY(), newZ);
 	}
 
+	/**
+	 * Converts the {@link BlockPos} to its position relative to the chunk it's in.
+	 *
+	 * @param pos the pos
+	 * @return the block pos
+	 */
 	public static BlockPos chunkPosition(BlockPos pos)
 	{
 		return new BlockPos(pos.getX() - (pos.getX() >> 4) * 16, pos.getY() - (pos.getY() >> 4) * 16, pos.getZ() - (pos.getZ() >> 4) * 16);
 	}
 
+	/**
+	 * Gets an iterable iterating through all the {@link BlockPos} intersecting the passed {@link AxisAlignedBB}.
+	 *
+	 * @param aabb the aabb
+	 * @return the all in box
+	 */
 	public static Iterable<BlockPos> getAllInBox(AxisAlignedBB aabb)
 	{
 		return BlockPos.getAllInBox(new BlockPos(aabb.minX, aabb.minY, aabb.minZ),
@@ -70,4 +91,32 @@ public class BlockPosUtils
 	{
 		return BlockPos.fromLong(buf.readLong());
 	}
+
+	/**
+	 * Compares the distance of the passed {@link BlockPos} to the origin (0,0,0).
+	 *
+	 * @param pos1 the pos 1
+	 * @param pos2 the pos 2
+	 * @return the int
+	 */
+	public static int compare(BlockPos pos1, BlockPos pos2)
+	{
+		return compare(new Point(0, 0, 0), pos1, pos2);
+	}
+
+	/**
+	 * Compares the distance of the passed {@link BlockPos} to the <i>offset</i> {@link Point}.
+	 *
+	 * @param offset the offset
+	 * @param pos1 the pos 1
+	 * @param pos2 the pos 2
+	 * @return the int
+	 */
+	public static int compare(Point offset, BlockPos pos1, BlockPos pos2)
+	{
+		if (pos1.equals(pos2))
+			return 0;
+		return pos1.distanceSq(offset.x, offset.y, offset.z) > pos2.distanceSq(offset.x, offset.y, offset.z) ? 1 : -1;
+	}
+
 }
