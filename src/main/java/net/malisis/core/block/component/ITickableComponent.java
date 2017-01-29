7a9757cdfd981483a0d72079c3d4d0efefb68dcf
Ordinastie <ordinastie@hotmail.com>
2017-01-29 16:26:42 +0100

    Updated comments

--- src/main/java/net/malisis/core/block/component/ITickableComponent.java
+++ src/main/java/net/malisis/core/block/component/ITickableComponent.java
@@ -28,23 +28,44 @@ import java.util.Random;
 
 import net.malisis.core.block.IBlockComponent;
 import net.malisis.core.block.IComponentProvider;
+import net.malisis.core.block.MalisisBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 /**
- * @author Ordinastie
+ * ITickableComponent is used for as callbacks for {@link PeriodicTickableComponent} and {@link RandomTickableComponent}.
  *
+ * @author Ordinastie
  */
 public interface ITickableComponent
 {
+	/**
+	 * Called when the block updates the component.
+	 *
+	 * @param block the block
+	 * @param world the world
+	 * @param pos the pos
+	 * @param state the state
+	 * @param rand the rand
+	 * @return the delay before the next tick, 0 to stop updates (unused for {@link RandomTickableComponent}).
+	 */
 	public int update(Block block, World world, BlockPos pos, IBlockState state, Random rand);
 
-	public class RandomTickableComponent implements ITickableComponent, IBlockComponent
+	/**
+	 * RandomTickableComponent when added to {@link MalisisBlock} allows the block to tick randomly.
+	 */
+	public class RandomTickableComponent implements IBlockComponent
 	{
+		/** The tickable callback used by this {@link RandomTickableComponent}. */
 		private final ITickableComponent tickable;
 
+		/**
+		 * Instantiates a new {@link RandomTickableComponent}.
+		 *
+		 * @param tickable the tickable
+		 */
 		private RandomTickableComponent(ITickableComponent tickable)
 		{
 			this.tickable = tickable;
@@ -57,25 +78,50 @@ public interface ITickableComponent
 				((Block) provider).setTickRandomly(true);
 		}
 
-		@Override
-		public int update(Block block, World world, BlockPos pos, IBlockState state, Random rand)
+		/**
+		 * Updates this component and calls the {@link ITickableComponent} callback.<br>
+		 * Called from {@link MalisisBlock#randomTick(World, BlockPos, IBlockState, Random)}.
+		 *
+		 * @param block the block
+		 * @param world the world
+		 * @param pos the pos
+		 * @param state the state
+		 * @param rand the rand
+		 * @return the int
+		 */
+		public void update(Block block, World world, BlockPos pos, IBlockState state, Random rand)
 		{
 			tickable.update(block, world, pos, state, rand);
-			return 0;
 		}
 	}
 
-	public class PeriodicTickableComponent implements ITickableComponent, IBlockComponent
+	/**
+	 * PeriodicTickableComponent when added to {@link MalisisBlock} allows the block to tick periodically.
+	 */
+	public class PeriodicTickableComponent implements IBlockComponent
 	{
+		/** The tickable callback used by this {@link PeriodicTickableComponent}. */
 		private final ITickableComponent tickable;
+		/** Delay before the first tick. */
 		private final int firstTickDelay;
 
+		/**
+		 * Instantiates a new {@link PeriodicTickableComponent} with the speicified delay before the first tick.
+		 *
+		 * @param tickable the tickable
+		 * @param firstTickDelay delay before the first tick, 0 to not start ticking automatically.
+		 */
 		public PeriodicTickableComponent(ITickableComponent tickable, int firstTickDelay)
 		{
 			this.tickable = tickable;
 			this.firstTickDelay = firstTickDelay;
 		}
 
+		/**
+		 * Instantiates a new {@link PeriodicTickableComponent} that will not start ticking automatically.
+		 *
+		 * @param tickable the tickable
+		 */
 		public PeriodicTickableComponent(ITickableComponent tickable)
 		{
 			this(tickable, 0);
@@ -88,7 +134,18 @@ public interface ITickableComponent
 				world.scheduleUpdate(pos, block, 1);
 		}
 
-		@Override
+		/**
+		 * Updates this component and calls the {@link ITickableComponent} callback.<br>
+		 * Called from {@link MalisisBlock#randomTick(World, BlockPos, IBlockState, Random)}.<br>
+		 * Return value sets the delay before the next tick, or 0 to stop ticking.
+		 *
+		 * @param block the block
+		 * @param world the world
+		 * @param pos the pos
+		 * @param state the state
+		 * @param rand the rand
+		 * @return the delay before the next tick, 0 to stop updates.
+		 */
 		public int update(Block block, World world, BlockPos pos, IBlockState state, Random rand)
 		{
 			return tickable.update(block, world, pos, state, rand);
