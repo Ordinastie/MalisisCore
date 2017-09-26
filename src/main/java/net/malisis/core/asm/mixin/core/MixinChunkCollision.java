2ffbb5a92f86fc329e4ff7ff66c794024f090da1
Ordinastie <ordinastie@hotmail.com>
2017-09-26 09:36:09 +0200

    Finished migrating ChunkCollisionTransformer to mixin Updated mixin to 0.7.4

--- src/main/java/net/malisis/core/asm/mixin/core/MixinChunkCollision.java
+++ src/main/java/net/malisis/core/asm/mixin/core/MixinChunkCollision.java
@@ -24,23 +24,34 @@
 
 package net.malisis.core.asm.mixin.core;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import net.malisis.core.util.Point;
 import net.malisis.core.util.chunkcollision.ChunkCollision;
+import net.malisis.core.util.chunkcollision.IChunkCollidable;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.network.play.client.CPacketPlayerDigging;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 /**
@@ -52,7 +63,11 @@ public class MixinChunkCollision
 	@Mixin(ItemBlock.class)
 	public static class MixinItemBlock
 	{
-		@Inject(method = "onItemUse", at = @At(value = "INVOKE", target = "getMetadata"), locals = LocalCapture.CAPTURE_FAILSOFT)
+		//When placing a block we need to check if it doesn't collide with a IChunkCollidable further away with a bigger bounding box
+		@Inject(method = "onItemUse",
+				at = @At(value = "INVOKE", target = "getMetadata"),
+				locals = LocalCapture.CAPTURE_FAILSOFT,
+				cancellable = true)
 		private void onOnItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ, CallbackInfoReturnable<EnumActionResult> cir, IBlockState iblockstate, Block block, ItemStack itemstack)
 		{
 			if (!ChunkCollision.get().canPlaceBlockAt(player, world, block, pos, hand, side))
@@ -63,25 +78,67 @@ public class MixinChunkCollision
 	@Mixin(NetHandlerPlayServer.class)
 	public static class MixinNetHandlerPlayServer
 	{
-		//private World world;
-		//private BlockPos pos;
+		private World world;
+		private BlockPos pos;
 
-		//		@Inject(method = "processPlayerDigging", at = @At(value = "STORE", ordinal = 4))
-		//		private void captureBlockPos(CPacketPlayerDigging packet, CallbackInfo ci)
-		//		{
-		//			pos = packet.ge
-		//		}
+		@Shadow
+		private MinecraftServer serverController;
+		@Shadow
+		public EntityPlayerMP player;
 
-		//		@ModifyVariable(method = "processPlayerDigging", at = @At(value = "STORE", ordinal = 4), require = 1, print = true)
-		//		private double onPlayerDigging(double distance)
-		//		{
-		//			//BlockPos pos = packet.getPosition();
-		//			//World world = ((NetHandlerPlayServer) (Object) this).player.world;
-		//
-		//			//if (world.getBlockState(pos).getBlock() instanceof IChunkCollidable)
-		//			//	return 0;
-		//
-		//			return distance;
-		//		}
+		@Inject(method = "processPlayerDigging", at = @At(value = "HEAD"))
+		private void captureBlockPos(CPacketPlayerDigging packet, CallbackInfo ci)
+		{
+			pos = packet.getPosition();
+			world = serverController.getWorld(this.player.dimension);
+		}
+
+		//Overrides reach distance check. Because the block currently being digged might be further away than expected
+		//depending on the size of its bounding box if it's a IChunkCollidable
+		@ModifyVariable(method = "processPlayerDigging",
+						ordinal = 3, // double d3, using index = 10
+						at = @At(value = "STORE", ordinal = 0), // first STORE of double d3
+						require = 1)
+		private double onPlayerDigging(double distance)
+		{
+			if (world.getBlockState(pos).getBlock() instanceof IChunkCollidable)
+				return 0;
+			return distance;
+		}
+	}
+
+	@Mixin(World.class)
+	public static class MixinWorld
+	{
+		private Pair<Point, Point> infos = null;
+
+		//because rayTraceBlocks mutate the src of the raytrace while iterating, when need to cache the parameters first
+		@Inject(method = "rayTraceBlocks(Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;ZZZ)Lnet/minecraft/util/math/RayTraceResult;",
+				at = @At(value = "HEAD"))
+		private void setInfos(Vec3d src, Vec3d dest, boolean stopOnLiquid, boolean ignoreBlockWithoutBoundingBox, boolean returnLastUncollidableBlock, CallbackInfoReturnable<RayTraceResult> cir)
+		{
+			if (src != null && dest != null)
+				infos = Pair.of(new Point(src), new Point(dest));
+			else
+				infos = null;
+		}
+
+		//before each return, we check if our rayTrace (against IChunkCollidable) yeild a result closer to src, if so, use that result
+		@Inject(method = "rayTraceBlocks(Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;ZZZ)Lnet/minecraft/util/math/RayTraceResult;",
+				at = @At(value = "RETURN"),
+				cancellable = true)
+		private void onRayTraceBlocks(Vec3d src, Vec3d dest, boolean stopOnLiquid, boolean ignoreBlockWithoutBoundingBox, boolean returnLastUncollidableBlock, CallbackInfoReturnable<RayTraceResult> cir)
+		{
+			if (infos == null)
+				return;
+
+			RayTraceResult result = ChunkCollision.get().getRayTraceResult(	(World) (Object) this,
+																			infos,
+																			cir.getReturnValue(),
+																			stopOnLiquid,
+																			ignoreBlockWithoutBoundingBox,
+																			returnLastUncollidableBlock);
+			cir.setReturnValue(result);
+		}
 	}
 }
